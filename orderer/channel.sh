efec9c830e5c721494c5897a9ab5ceb016bcac98
Productzilla <hello@productzillaacademmy.com>
2023-04-27 16:00:31 +0700

    fix flow for create channel

--- orderer/channel.sh
+++ orderer/channel.sh
@@ -1,13 +1,16 @@
+$CONTAINER
 function createChannel() {
   # Bring up the network if it is not already up.
   bringUpNetwork="false"
-  
-  if [[ ! -d "organizations/peerOrganizations" ]]; then
-    echo "Bringing network down to sync certs with containers"
-    make orderer-down
+
+  if ! $CONTAINER_CLI info > /dev/null 2>&1 ; then
+    fatalln "$CONTAINER_CLI network is required to be running to create a channel"
   fi
 
-  [[ ! -d "organizations/peerOrganizations" ]] && bringUpNetwork="true" || echo "Network Running Already"
+  # check if all containers are present
+  len=$($CONTAINER_CLI ps | grep hyperledger/ | wc -l)
+
+  [[ $len -lt 2 ]] && bringUpNetwork="true" || echo "Network Running Already"
 
   if [ $bringUpNetwork == "true"  ]; then
     echo "Bringing up network"
